0d9607f98c8ed315206b0df9774354f0920c98c8
ZZFlying <wuya41@qq.com>
2020-04-28 23:28:27 +0800

    feature 修改持久化临时文件夹实现

--- src/temp_folder.py
+++ src/temp_folder.py
@@ -1,48 +1,20 @@
 #!/usr/bin/env python3
-import string as _string
-import shutil as _shutil
-import weakref as _weakref
-import warnings as _warnings
-from random import sample as _sample
-from os import path as _path, getcwd as _getcwd, mkdir as _mkdir
+from shutil import rmtree
+from warnings import warn
+from tempfile import TemporaryDirectory
 
 
-def mkdtemp(suffix=None, prefix=None, path=None):
-    _count = 0
-    _candidate = _string.ascii_letters + _string.digits
-    _letter_list = [i for i in _candidate]
-    while True:
-        if _count > 10000:
-            raise FileExistsError
-        _name = ''.join(_sample(_letter_list, 7))
-        if suffix:
-            _name = _name + suffix
-        if prefix:
-            _name = prefix + _name
-        if path:
-            _dir = _path.join(path, _name)
-        else:
-            _dir = _path.join(_getcwd(), _name)
-        if _path.exists(_dir):
-            _count += 1
-            continue
-        _mkdir(_dir)
-        return _dir
+class AutoTempDir(TemporaryDirectory):
 
+    def __init__(self, suffix=None, prefix=None, dir=None, auto_del=True):
+        TemporaryDirectory.__init__(self, suffix, prefix, dir)
+        self._del = auto_del
 
-class AutoTempdir(object):
-    # 生成临时文件夹，根据设置决定失去失去引用后是否删除
-    def __init__(self, suffix=None, prefix=None, dir=None, autodel=True):
-        self.name = mkdtemp(suffix, prefix, dir)
-        self._autodel = autodel
-        self._finalizer = _weakref.finalize(
-            self, self._cleanup, self.name,
-            warn_message="Implicitly cleaning up {!r}".format(self))
+    @classmethod
+    def _cleanup(cls, name, warn_message):
+        rmtree(name)
+        warn(warn_message, ResourceWarning)
 
-    def _cleanup(self, name, warn_message):
-        if self._autodel:
-            _shutil.rmtree(name)
-            _warnings.warn(warn_message, ResourceWarning)
-
-    def __repr__(self):
-        return "<{} {!r}>".format(self.__class__.__name__, self.name)
+    def cleanup(self):
+        if self._finalizer.detach() and self._del:
+            rmtree(self.name)
