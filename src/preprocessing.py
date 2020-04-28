ee8f0ea46dbccf8effd54394458d969c6eab1444
ZZFlying <wuya41@qq.com>
2020-04-28 22:04:01 +0800

    feature 添加gz压缩格式支持

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,29 +1,36 @@
 #!/usr/bin/env python3
-import gzip
 import logging
-from os import path, mkdir
+from os import path, mkdir, system
+from concurrent.futures import ThreadPoolExecutor, as_completed
+
+from src.fastqReader import fastqReader
+from src.singleton_config import Config
+
+
+def create_handle(file_handles, read_count, tempdir, barcode='Mismatch', ident='Mismatch'):
+    filename = path.join(tempdir, ident + '_{}.fq')
+    fw_file = filename.format('FW')
+    rc_file = filename.format('RC')
+    fw = open(fw_file, 'wt')
+    rc = open(rc_file, 'wt')
+    file_handles[barcode] = (fw, rc)
+    read_count[barcode] = 0
+    return [fw_file, rc_file]
 
 
 def create_output_files(work_dir, barcodes, mismatch):
+    files_list = list()
+    read_count = dict()
     file_handles = dict()
-    read_seqs = dict()
     tempdir = path.join(work_dir, 'PreprocessedFiles')
     if not path.exists(tempdir):
         mkdir(tempdir)
     # 创建每个样本对应FW和RC的文件io
     for (barcode, ident) in barcodes.items():
-        filename = path.join(tempdir, ident)
-        fw = open(filename + '_FW.fq', 'w')
-        rc = open(filename + '_RC.fq', 'w')
-        file_handles[ident] = fw, rc
-        read_seqs[ident] = 0
+        files_list.extend(create_handle(file_handles, read_count, tempdir, barcode, ident))
     if mismatch:
-        filename = path.join(tempdir, 'Mismatch')
-        fw = open(filename + '_FW.fq', 'w')
-        rc = open(filename + '_RC.fq', 'w')
-        file_handles['Mismatch'] = fw, rc
-        read_seqs['Mismatch'] = 0
-    return file_handles, read_seqs
+        files_list.extend(create_handle(file_handles, read_count, tempdir))
+    return file_handles, read_count, files_list
 
 
 def write_summary(work_dir, read_seqs, total):
@@ -33,67 +40,68 @@ def write_summary(work_dir, read_seqs, total):
         logging.info('Printing Summary to SummaryFile.txt')
         for (ident, count) in read_seqs.items():
             match += count
-            record = '{}\t{}\n'.format(ident, count)
+            record = '{}    {}\n'.format(ident, count)
             output.write(record)
         mismatch = total - match
-        mismatch = 'Mismatches\t{}\nTotal\t{}\n'.format(mismatch, total)
+        mismatch = 'Mismatches  {}\nTotal   {}\n'.format(mismatch, total)
         output.write(mismatch)
 
 
 def close_file(file_handles):
     for (file, handle) in file_handles.items():
-        fw, rc = handle
+        (fw, rc) = handle
         fw.close()
         rc.close()
 
 
+def write_record(handles, barcode, fw_read, rc_read):
+    (fw_handle, rc_handle) = handles[barcode]
+    fw_handle.write(fw_read.parse(0))
+    rc_handle.write(rc_read.parse(1))
+
+
+def compress(filename):
+    cmd = 'gzip -f {}'.format(filename)
+    system(cmd)
+    return filename
+
+
+def compress_multi(files_list):
+    threads = Config()['thread']
+    pool = ThreadPoolExecutor(threads)
+    result = list()
+    for file in files_list:
+        logging.info('Gzip  {}'.format(file))
+        result.append(pool.submit(compress, file))
+    for future in as_completed(result):
+        gz_file = future.result()
+        gz_file = gz_file + '.gz'
+        logging.info(gz_file + ' had created.')
+
+
 def main(fw_file, rc_file, work_dir, barcodes, length, mismatch):
     try:
-        _open = gzip.open if fw_file.split('.')[-1] == 'gz' else open
-        with _open(fw_file, 'rt') as fw, _open(rc_file, 'rt') as rc:
-            logging.info('STEP 1: PREPROCESSING OF FASTA FILES STARTED')
-
+        logging.info('STEP 1: PREPROCESSING OF FASTA FILES STARTED')
+        with fastqReader(fw_file) as fw_in, fastqReader(rc_file) as rc_in:
+            total = 0
             fw_filename = path.basename(fw_file)
             rc_filename = path.basename(rc_file)
+            file_handles, read_count, files_list = create_output_files(work_dir, barcodes, mismatch)
             logging.info('Reading sequence files:{} and {}'.format(fw_filename, rc_filename))
-
-            total = 0
-            fwd_line = fw.readline()
-            rev_line = rc.readline()
-            out_file_handles, read_seqs = create_output_files(work_dir, barcodes, mismatch)
-            while fwd_line and rev_line:
-                if fwd_line.startswith('@') and rev_line.startswith('@'):
-                    total += 1
-
-                    fwd_id = '{}#0_0\n'.format(fwd_line.split()[0])
-                    fwd_bar = fwd_line.strip()[-length:]
-                    fwd_seq = fw.readline()
-                    fwd_plus = fw.readline()
-                    fwd_qual = fw.readline()
-
-                    rev_id = '{}#0_1\n'.format(rev_line.split()[0])
-                    rev_bar = rev_line.strip()[-length:]
-                    rev_seq = rc.readline()
-                    rev_plus = rc.readline()
-                    rev_qual = rc.readline()
-                    # 根据读取的barcode获得对应样本的文件io
-                    # 输出读取的序列到对应的文件
-                    if fwd_bar in barcodes and fwd_bar == rev_bar:
-                        out_fw, out_rc = out_file_handles[barcodes[fwd_bar]]
-                        out_fw.writelines([fwd_id, fwd_seq, fwd_plus, fwd_qual])
-                        out_rc.writelines([rev_id, rev_seq, rev_plus, rev_qual])
-                        read_seqs[barcodes[fwd_bar]] += 1
-                    elif mismatch:
-                        # 输出无法匹配任何样本的序列
-                        out_fw, out_rc = out_file_handles['Mismatch']
-                        out_fw.writelines([fwd_id, fwd_seq, fwd_plus, fwd_qual])
-                        out_rc.writelines([rev_id, rev_seq, rev_plus, rev_qual])
-                        read_seqs['Mismatch'] += 1
-
-                fwd_line = fw.readline()
-                rev_line = rc.readline()
+            for (fw_read, rc_read) in zip(fw_in, rc_in):
+                total += 1
+                barcode = fw_read.barcode
+                if barcode in barcodes and fw_read.barcode == rc_read.barcode:
+                    write_record(file_handles, barcode, fw_read, rc_read)
+                    read_count[barcode] += 1
+                elif mismatch:
+                    # 输出无法匹配任何样本的序列
+                    write_record(file_handles, 'Mismatch', fw_read, rc_read)
+                    read_count['Mismatch'] += 1
             # 安全关闭文件io，保障所有序列的正常写入文件
-            close_file(out_file_handles)
-            write_summary(work_dir, read_seqs, total)
+            close_file(file_handles)
+            write_summary(work_dir, read_count, total)
+            if Config()['gzip']:
+                compress_multi(files_list)
     except FileNotFoundError:
         logging.error('Failed to open file')
