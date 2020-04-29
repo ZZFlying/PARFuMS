6158abfa39fc538c6acaa8ea1a6f98de073f3ec9
ZZFlying <wuya41@qq.com>
2020-04-29 22:01:41 +0800

    fix 修改参数名

--- util/merge_split.py
+++ util/merge_split.py
@@ -3,10 +3,10 @@ import logging
 from os import path
 
 
-def merge_file(work_dir, cm_files, suffix):
+def merge_file(work_dir, split_files, suffix):
     logging.info('Merging clean fasta output files')
     result = dict()
-    for (ident, files) in cm_files.items():
+    for (ident, files) in split_files.items():
         out_file = path.join(work_dir, ident, '{}.{}'.format(ident, suffix))
         result[ident] = out_file
         with open(out_file, 'w') as out:
